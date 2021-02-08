0e244e09ef8e2e0389a822a05c9d4efac1be079c
EAD\dvu36 <vuhoaiduc123@gmail.com>
2021-02-08 16:54:58 +0700

    Test 2

--- routes/admin.js
+++ routes/admin.js
@@ -8,5 +8,5 @@ const path = require('path')
 
 router.get('/add-product',productsController.getAddProduct);
 router.post('/add-product',productsController.postAddProduct);
-
+///
 module.exports = router;
